52b406b9ceb703590268fe7b38df0092b68d0b0d
diablox36 <thomas.rivest36@gmail.com>
2024-03-28 11:10:41 -0400

    test du login

--- login.js
+++ login.js
@@ -1,34 +1,55 @@
-const buttonLogin = document.querySelector("#login")
+const btnLogin = document.querySelector("#login")
+const txtEmail = document.querySelector("#email")
+const txtPassword = document.querySelector("#password")
 
-buttonLogin.addEventListener('click', (event) => {
 
-    event.preventDefault();
-    const form = document.querySelector('form');
-    const message = document.querySelector(".erreur");
+btnLogin.addEventListener('click', async (event) => {
+    event.preventDefault()
 
-    let formData = new FormData(form);
-    let userType = formData.get("#proprietaire").checked ? "propriétaire" : "locataire"
-
-    let connectingUser = {
-        email: formData.get("email"),
-        password: formData.get("password")
+    const user = {
+        adresse_courriel: txtEmail.textContent,
+        mot_de_passe: txtPassword.textContent
     }
 
-    user = getUser(connectingUser.email);
-
-    if (user != false && user.mot_de_passe == connectingUser.password) {
-        //implmenter connexion et autre truc du genre
-        location.replace("./index.html");
-    }
-    else {
-        message.textContent = "Email invalide!"
-    }
+    const response = await fetch('https://equipe500.tch099.ovh/projet2/api/userexist', {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(user)
+    })
+    const txt = await response.json();
+    console.log(txt)
 })
 
-async function getUser(email) {
-    const response = await fetch('https://equipe500.tch099.ovh/projet2/api/getutilisateur/' + email);
+// location.replace("./login.html")
 
-    return response != [] ? response.json() : false
 
-}
+//     event.preventDefault();
+//     const form = document.querySelector('form');
+//     const message = document.querySelector(".erreur");
+
+//     let formData = new FormData(form);
+//     let userType = formData.get("#proprietaire").checked ? "propriétaire" : "locataire"
+
+//     let connectingUser = {
+//         email: formData.get("email"),
+//         password: formData.get("password")
+//     }
+
+//     user = getUser(connectingUser.email);
+
+//     if (user != false && user.mot_de_passe == connectingUser.password) {
+//         //implmenter connexion et autre truc du genre
+//         location.replace("./index.html");
+//     }
+//     else {
+//         message.textContent = "Email invalide!"
+//     }
+// })
+
+// async function getUser(email) {
+//     const response = await fetch('https://equipe500.tch099.ovh/projet2/api/getutilisateur/' + email);
+
+//     return response != [] ? response.json() : false
+
+// }
 
