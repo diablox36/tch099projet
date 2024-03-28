ea394cb9754b441aff964e30302c2c5adae96ed8
diablox36 <thomas.rivest36@gmail.com>
2024-03-28 13:37:27 -0400

    test undefined

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ async function fetchAppartements() {
     const responseImage = await fetch("https://equipe500.tch099.ovh/projet2/api/getfirstimage/" + appartement.id)
     const images = await responseImage.json();
 
+    console.log(images[0]['image_url'] == undefined)
+    console.log(isset(images[0]['image_url']))
     if(images[0]['image_url'] == undefined) {
       listeAppartements.push([appartement, "https://via.placeholder.com/200"])
     }
