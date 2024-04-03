f0c6874315b46407fc01a1505d0aab118d5c88ac
diablox36 <thomas.rivest36@gmail.com>
2024-04-03 14:22:06 -0400

    inscription

--- inscription.js
+++ inscription.js
@@ -1,24 +1,26 @@
+const email = document.querySelector("#email")
+const nom = document.querySelector("#nom")
+const password = document.querySelector("#password")
+const tel = document.querySelector("#tel")
+const radio = document.querySelector("#proprietaire")
+
 // Pour enregistrer un nouvel utilisateur
 document.querySelector("#inscription").addEventListener('click', async (event) => {
-    let form = document.querySelector('form');
-    let formData = new FormData(form);
-
-    let newUser = {
-        adresse_courriel: formData.get("email"),
-        prenom: formData.get("nom").split(' ')[0],
-        nom: formData.get("nom").split(' ')[1],
-        mot_de_passe: formData.get("password"),
-        telephone: formData.get("tel"),
-        type_compte: formData.get("proprietaire").checked ? "propriétaire" : "locataire"
+    let utilisateur = {
+        adresse_courriel: email.value,
+        prenom: nom.value.split(' ')[0],
+        nom: nom.value.split(' ')[1],
+        mot_de_passe: password.value,
+        telephone: tel.value,
+        type_compte: radio.checked ? "propriétaire" : "locataire"
     }
-
+    
     await fetch('https://equipe500.tch099.ovh/projet2/api/ajouterutilisateur',
-        {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(newUser)
-        })
-
+    {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(utilisateur)
+    })
+    
     location.replace("./login.html")
-
 })
